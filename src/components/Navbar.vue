16a990626abcd87455fc0cd52a36f24ecb90c671
rkrajniak <robinpatrickkrajniak@gmail.com>
2022-06-05 15:29:50 +0200

    m3 done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Tagebuch App </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <router-link class="nav-link" to="/">Home</router-link>
+        <router-link class="nav-link" to="/about">About</router-link>
+        <router-link class="nav-link" to="/tagebucheintrag">Tagebucheintrag</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav-bar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Tagebucheintrag.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Welcome to Tagebucheintrag</h1>
+</template>
+
+<script>
+export default {
+  name: 'Tagebuch-eintrag'
+}
+</script>
+
+<style scoped>
+
+</style>
