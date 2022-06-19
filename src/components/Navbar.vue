9ff2a924ac9f74aa851f931eb15f9192e5c7828c
rkrajniak <robinpatrickkrajniak@gmail.com>
2022-06-19 14:12:52 +0200

    add github action for testing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">My Tagebuch App </a>
+      <a class="navbar-brand" href="#">My Tagebuch App</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
